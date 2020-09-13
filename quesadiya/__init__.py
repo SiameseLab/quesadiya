2e80065200be6cc23292ff57cc094ebde373f340
box-key <kei.box.fight@gmail.com>
2020-09-13 11:47:44 -0400

    added line to get base path

--- quesadiya/__init__.py
+++ quesadiya/__init__.py
@@ -1,4 +1,12 @@
 import pkg_resources
+import os
 
 
+# version info of this package
 __version__ = pkg_resources.get_distribution("quesadiya").version
+# get root path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def show_path():
+    return BASE_DIR
