13e5a7f69f4779019854c9186b8ed414d74a82c0
box-key <kei.box.fight@gmail.com>
2020-09-17 23:34:17 -0400

    added 'expire_on_commit' arguemnt to __call__ method

--- quesadiya/db/session.py
+++ quesadiya/db/session.py
@@ -16,9 +16,18 @@ class SessionContextManager:
         self.session_maker = session_maker
 
     @contextmanager
-    def __call__(self):
-        """Provide a transactional scope around a series of operations."""
-        session = self.session_maker()
+    def __call__(self, expire_on_commit):
+        """Provide a transactional scope around a series of operations.
+
+        Notes
+        -----
+        * Default value of `expire_on_commit` is True, so it's preferred unless
+          you have any specific reason not to expire session on commit. For
+          example, you might want to set it False for get methods. For
+          transactional operations (create, delete), it'd be better to set it
+          True.
+        """
+        session = self.session_maker(expire_on_commit=expire_on_commit)
         try:
             # enforce foreign key constraints
             session.execute("PRAGMA foreign_keys = ON;")
