94c231208d193b25ed0fc3fc76107a70fe68d9b2
box-key <kei.box.fight@gmail.com>
2020-10-17 20:57:39 -0400

    implemented run command with port option; added test code for run

--- quesadiya/commands/run.py
+++ quesadiya/commands/run.py
@@ -11,7 +11,10 @@ import os
 import subprocess
 
 
-def operator():
+def operator(port_number):
     admin_interface = factory.get_admindb_interface()
     run_path = os.path.join(quesadiya.get_base_path(), 'django_tool', 'manage.py')
-    subprocess.call([run_path, "runserver"], shell=True)
+    subprocess.call(
+        [run_path, "runserver", "localhost:{}".format(port_number), "--insecure"],
+        shell=True
+    )
