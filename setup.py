71aea2f8eb3b7051a0a34fd5460ba5407de428d6
box-key <kei.box.fight@gmail.com>
2020-11-05 09:11:28 -0500

    updated version for official release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_include_files(directory):
     return [os.path.join("..", path) for path in files]
 
 
-__version__ = "0.3"
+__version__ = "1.0"
 
 
 with open(os.path.join(os.path.dirname(__file__), "README.rst"), "r") as f:
